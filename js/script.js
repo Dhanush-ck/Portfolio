a482a91fac6e3daea5c46b4993965b5f0deb0f14
Dhanush CK <dhanushck01@gmail.com>
2024-04-04 00:29:56 +0530

    Added JS File

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop <windowheight - revealPoint) {
+            if(i==0){
+                reveals[i].classList.add('slideTop-50px');
+            }
+            else if(i==1) {
+                reveals[i].classList.add('slideLeft');
+            }
+        }
+        else {
+            reveals[i].classList.remove('slideTop-50px');
+            reveals[i].classList.remove('slideLeft');
+        }
+    }
+}
